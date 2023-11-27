26ff08c1e68bbe5f07f86828c945d609b20e4b27
egasa21 <esaputra168@gmail.com>
2023-11-27 21:56:06 +0700

    FEAT : Create SignUpViewModel and implement viewmodel into SignUpScreen

--- app/src/main/java/com/aspald/aspald/presentation/signup/SignUpScreen.kt
+++ app/src/main/java/com/aspald/aspald/presentation/signup/SignUpScreen.kt
@@ -1,6 +1,7 @@
 package com.aspald.aspald.presentation.signup
 
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,41 +9,59 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import com.aspald.aspald.R
+import com.aspald.aspald.presentation.components.ButtonComponent
+import com.aspald.aspald.presentation.components.MyEmailTextField
 import com.aspald.aspald.presentation.components.MyTextField
 import com.aspald.aspald.presentation.components.MyTypography
 import com.aspald.aspald.presentation.components.PasswordTextField
-import com.aspald.aspald.presentation.signin.SignInScreen
+import com.aspald.aspald.presentation.navgraph.Route
+import com.aspald.aspald.utils.UiState
 
 @Composable
-fun SignUpScreen() {
+fun SignUpScreen(navController: NavController, signUpViewModel: SignUpViewModel = hiltViewModel()) {
+    var name by remember{ mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var confirmPassword by remember { mutableStateOf("") }
     var passwordVisibility by remember { mutableStateOf(false) }
     var confirmPasswordVisibility by remember { mutableStateOf(false) }
 
+    var nameError by remember { mutableStateOf(false) }
     var emailError by remember { mutableStateOf(false) }
     var passwordError by remember { mutableStateOf(false) }
     var confirmPasswordError by remember { mutableStateOf(false) }
+
+    val uiState by signUpViewModel.uiState.collectAsState()
+    val scrollState = rememberScrollState()
+
+    LaunchedEffect(uiState){
+        if (uiState is UiState.Success){
+            navController.navigate(Route.SignInScreen.route){
+                popUpTo(Route.SignUpScreen.route) { inclusive = true}
+            }
+        }
+    }
 
     MaterialTheme(
         typography = MyTypography
@@ -50,11 +69,12 @@ fun SignUpScreen() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp),
+                .padding(16.dp)
+                .verticalScroll(scrollState),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
 
-            Spacer(modifier = Modifier.height(160.dp))
+            Spacer(modifier = Modifier.height(100.dp))
 
             Column(
                 modifier = Modifier.align(Alignment.Start)
@@ -73,6 +93,20 @@ fun SignUpScreen() {
             Spacer(modifier = Modifier.height(50.dp))
 
             MyTextField(
+                labelValue = "name",
+                onValueChange = {
+                    name = it
+                    nameError = false // Reset error when user changes the email
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                errorStatus = nameError,
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            MyEmailTextField(
                 labelValue = "email",
                 onValueChange = {
                     email = it
@@ -112,23 +146,20 @@ fun SignUpScreen() {
 
             )
 
-            Button(
+
+            ButtonComponent(
                 onClick = {
-                    // TODO: Handle sign up logic
-                    //handleSignUp(email, password, confirmPassword)
+                    if (password == confirmPassword){
+                        Log.d("Coba", "$name, $email, $password, $confirmPassword")
+                        signUpViewModel.signUp(name, email, password, confirmPassword)
+                    }else {
+                        emailError = email.isEmpty()
+                        // You might want to handle password error similarly
+                    }
                 },
-                shape = RoundedCornerShape(12.dp),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.primary))
-            ) {
-                Text(
-                    text = "Sign Up",
-                    style = MaterialTheme.typography.titleMedium,
-                    color = colorResource(id = R.color.secondary)
-                )
-            }
+                buttonText = "Sign Up"
+            )
+
 
             Spacer(modifier = Modifier.height(24.dp))
 
@@ -145,10 +176,8 @@ fun SignUpScreen() {
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-fun LoginComposablePreview(){
-    Surface(color = Color.White) {
-        SignUpScreen()
-    }
+fun SignUpScreenPreview() {
+    SignUpScreen(navController = NavController(LocalContext.current)) // You need to provide a NavController here.
 }
--- app/src/main/java/com/aspald/aspald/presentation/signup/SignUpViewModel.kt
+++ app/src/main/java/com/aspald/aspald/presentation/signup/SignUpViewModel.kt
@@ -1,4 +1,36 @@
 package com.aspald.aspald.presentation.signup
 
-class SignUpViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aspald.aspald.data.AuthRepository
+import com.aspald.aspald.utils.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val authRepository: AuthRepository,
+) : ViewModel() {
+    private val _uiState = MutableStateFlow<UiState>(UiState.Loading)
+    val uiState: StateFlow<UiState> = _uiState
+
+    fun signUp(name: String, email: String, password: String, confirmPassword: String) {
+        viewModelScope.launch {
+            if (password == confirmPassword) {
+                _uiState.value = UiState.Loading
+                val result = authRepository.signUp(name, email, password)
+                if (result.isSuccess) {
+                    _uiState.value = UiState.Success(null)
+                } else {
+                    _uiState.value = UiState.Error(result.exceptionOrNull()?.message ?: "Unknown Error")
+                }
+            } else {
+                // Passwords do not match
+                _uiState.value = UiState.Error("Passwords do not match")
+            }
+        }
+    }
 }
