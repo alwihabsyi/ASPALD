ec9c33725a86511143a708c9f97b26179c3184f8
egasa21 <esaputra168@gmail.com>
2023-11-27 21:53:48 +0700

    FEAT : add signup function

--- app/src/main/java/com/aspald/aspald/data/AuthRepository.kt
+++ app/src/main/java/com/aspald/aspald/data/AuthRepository.kt
@@ -2,6 +2,7 @@ package com.aspald.aspald.data
 
 import com.aspald.aspald.data.api.ApiServices
 import com.aspald.aspald.data.model.SignInRequest
+import com.aspald.aspald.data.model.SignUpRequest
 import com.aspald.aspald.utils.UserPreferences
 import javax.inject.Inject
 
@@ -19,6 +20,22 @@ class AuthRepository @Inject constructor(
                 } ?: Result.failure(Exception("Error logging in: Empty body"))
             } else {
                 Result.failure(Exception("Error logging in: ${response.message()}"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun signUp(name: String, email: String, password: String): Result<String> {
+        return try {
+            val response = apiServices.signUp(SignUpRequest(name, email, password))
+            if (response.isSuccessful && response.body() != null) {
+                response.body()?.let {
+                    // You can handle the success response here if needed
+                    Result.success(it.message)
+                } ?: Result.failure(Exception("Error signing up: Empty body"))
+            } else {
+                Result.failure(Exception("Error signing up: ${response.message()}"))
             }
         } catch (e: Exception) {
             Result.failure(e)
