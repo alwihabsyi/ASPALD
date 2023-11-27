ffca93b1db5c85bc862602865550508a3fbd6308
egasa21 <esaputra168@gmail.com>
2023-11-27 21:57:46 +0700

    UPDATE : add sign in and signup UI State

--- app/src/main/java/com/aspald/aspald/utils/UiState.kt
+++ app/src/main/java/com/aspald/aspald/utils/UiState.kt
@@ -7,4 +7,6 @@ sealed interface UiState {
     data class Success(val location: LatLng?): UiState
     data object RevokedPermissions: UiState
     data class Error(val message: String) : UiState
+    data object SignIn : UiState // Added for navigation to Sign In screen
+    data object SignUp : UiState // Added for navigation to Sign Up screen
 }
